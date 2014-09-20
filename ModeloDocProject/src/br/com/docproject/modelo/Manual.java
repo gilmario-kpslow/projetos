0a7ed7ca0fe435187e75666b69a0c1fd09df3bdb
gilmario-kpslow <gilmariooftware.com>
2014-09-20 06:35:55 -0300

    Merge branch 'master' of https://github.com/aricomputacao/Atus
    Conflicts:
    	AtusModelo/src/br/com/atus/enumerated/Sexo.java
    	AtusModelo/src/br/com/atus/modelo/Cliente.java
    	AtusModelo/src/br/com/atus/modelo/Pessoa.java
    	atus/src/java/br/com/atus/controller/CidadeController.java
    	atus/src/java/br/com/atus/controller/UnidadeFederativaController.java
    	atus/src/java/br/com/atus/dao/CidadeDAO.java
    	atus/src/java/br/com/atus/dao/UnidadeFederativaDAO.java
    	atus/src/java/br/com/atus/managedbean/ClienteMB.java
    	atus/src/java/br/com/atus/managedbean/WebServiceMB.java
    	atus/web/01/cliente.xhtml
    	atus/web/home.xhtml

---
+++ ModeloDocProject/src/br/com/docproject/modelo/Manual.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.docproject.modelo;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ *
+ * @author gilmario
+ */
+@Entity
+@Table(name = "manual")
+public class Manual implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Column(length = 50, nullable = false)
+    private String nome;
+    @NotBlank
+    @Column(length = 100, nullable = false)
+    private String nomeSistema;
+    @NotNull
+    @Column(length = 255)
+    private String observacao;
+    @NotNull
+    @Column(length = 100)
+    private String rodape;
+    @NotNull
+    @Column(length = 20)
+    private String versao;
+
+    public Manual() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVersao() {
+        return versao;
+    }
+
+    public void setVersao(String versao) {
+        this.versao = versao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeSistema() {
+        return nomeSistema;
+    }
+
+    public void setNomeSistema(String nomeSistema) {
+        this.nomeSistema = nomeSistema;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public String getRodape() {
+        return rodape;
+    }
+
+    public void setRodape(String rodape) {
+        this.rodape = rodape;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Manual other = (Manual) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+}
